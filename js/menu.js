044d89f31767acf3a3e3c1bc5387e77cb4fcce54
Sveta Konstantinova <svetikons@mail.ru>
2024-07-06 13:26:12 +0300

    change pages

---
+++ js/menu.js
@@ -0,0 +1,13 @@
+document.querySelector('.blocks__services').addEventListener('click', function (event) {
+    event.preventDefault();
+    document.querySelector('.menu').classList.toggle('hidden');
+});
+
+document.querySelector('.menu__close').addEventListener('click', function (event) {
+    event.preventDefault();
+    document.querySelector('.menu').classList.add('slide-out');
+    setTimeout(function () {
+        document.querySelector('.menu').classList.remove('slide-out');
+        document.querySelector('.menu').classList.add('hidden');
+    }, 1000);
+});
